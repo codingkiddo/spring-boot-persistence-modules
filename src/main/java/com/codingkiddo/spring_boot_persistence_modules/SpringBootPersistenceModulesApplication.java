0592d6f7fada8d9f4d00ee545f833dd103b9d84d
codingkiddo <codingkiddo@gmail.com>
2025-06-15 15:28:49 +0530

    first commit

---
+++ src/main/java/com/codingkiddo/spring_boot_persistence_modules/SpringBootPersistenceModulesApplication.java
@@ -0,0 +1,13 @@
+package com.codingkiddo.spring_boot_persistence_modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPersistenceModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPersistenceModulesApplication.class, args);
+	}
+
+}
