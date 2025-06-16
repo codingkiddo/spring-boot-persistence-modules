65e194c72994885e4e0b16b71f487146b673371f
codingkiddo <codingkiddo@gmail.com>
2025-06-16 19:54:24 +0530

    renamed:    src/main/java/com/codingkiddo/spring_boot_persistence_modules/data/jpa/listrepositories/lastrecord/Post.java -> src/main/java/com/codingkiddo/spring_boot_persistence_modules/spring/data/jpa/listrepositories/lastrecord/Post.java 	renamed:    src/main/java/com/codingkiddo/spring_boot_persistence_modules/data/jpa/listrepositories/lastrecord/PostRepository.java -> src/main/java/com/codingkiddo/spring_boot_persistence_modules/spring/data/jpa/listrepositories/lastrecord/PostRepository.java 	new file:   src/main/java/com/codingkiddo/spring_boot_persistence_modules/spring/data/jpa/naturalid/SpringBootNaturalIdApplication.java

--- src/main/java/com/codingkiddo/spring_boot_persistence_modules/spring/data/jpa/listrepositories/lastrecord/PostRepository.java
+++ src/main/java/com/codingkiddo/spring_boot_persistence_modules/spring/data/jpa/listrepositories/lastrecord/PostRepository.java
@@ -1,4 +1,4 @@
-package com.codingkiddo.spring_boot_persistence_modules.data.jpa.listrepositories.lastrecord;
+package com.codingkiddo.spring_boot_persistence_modules.spring.data.jpa.listrepositories.lastrecord;
 
 import java.util.List;
 
