c79552ded3b5c401e18b6d1f4fdea61f94ae5434
codingkiddo <codingkiddo@gmail.com>
2025-06-19 12:15:34 +0530

    modified:   src/main/java/com/codingkiddo/spring_boot_persistence_modules/spring/data/jpa/naturalid/repository/NaturalIdRepositoryImpl.java 	modified:   src/main/java/com/codingkiddo/spring_boot_persistence_modules/spring/data/jpa/naturalid/service/HotelRoomsService.java 	new file:   src/main/java/com/codingkiddo/spring_boot_persistence_modules/spring/data/jpa/naturalid/service/NaturalIdRepositoryConfig.java

--- src/main/java/com/codingkiddo/spring_boot_persistence_modules/spring/data/jpa/naturalid/repository/NaturalIdRepositoryImpl.java
+++ src/main/java/com/codingkiddo/spring_boot_persistence_modules/spring/data/jpa/naturalid/repository/NaturalIdRepositoryImpl.java
@@ -1,13 +1,15 @@
 package com.codingkiddo.spring_boot_persistence_modules.spring.data.jpa.naturalid.repository;
 
 import java.io.Serializable;
+import java.util.Optional;
 
+import org.hibernate.Session;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import jakarta.persistence.EntityManager;
 
-public class NaturalIdRepositoryImpl<T, ID extends Serializable> extends SimpleJpaRepository<T, ID> {
+public class NaturalIdRepositoryImpl<T, ID extends Serializable> extends SimpleJpaRepository<T, ID> implements NaturalIdRepository<T, ID> {
 
 	private final EntityManager entityManager;
 
@@ -16,4 +18,10 @@ public class NaturalIdRepositoryImpl<T, ID extends Serializable> extends SimpleJ
 		this.entityManager = entityManager;
 	} 
 
+	@Override
+    public Optional<T> naturalId(ID naturalId) {
+        return entityManager.unwrap(Session.class)
+            .bySimpleNaturalId(this.getDomainClass())
+            .loadOptional(naturalId);
+    }
 }
